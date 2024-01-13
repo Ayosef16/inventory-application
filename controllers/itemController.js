4e83efc349639cf9fb51cc5b4a98d3b6e54f6063
Ayosef <101937673+Ayosef16@users.noreply.github.com>
2024-01-13 12:09:35 -0300

    Create the index controller

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,13 +1,25 @@
 const Item = require("../models/item");
+const Category = require("../models/category");
+const Seller = require("../models/seller");
 
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Index");
+  const [itemCount, categoryCount, sellerCount] = await Promise.all([
+    Item.countDocuments().exec(),
+    Category.countDocuments().exec(),
+    Seller.countDocuments().exec(),
+  ]);
+  res.render("index", {
+    title: "Inventory Application Home",
+    item_count: itemCount,
+    category_count: categoryCount,
+    seller_count: sellerCount,
+  });
 });
 
 exports.item_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item list");
+  res.render("item_list", { title: "Item List" });
 });
 
 exports.item_detail = asyncHandler(async (req, res, next) => {
